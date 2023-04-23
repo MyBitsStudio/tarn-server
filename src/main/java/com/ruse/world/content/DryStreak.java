207819b59d0fa4dc0724f98380efa6ddd249e178
Mutant <69111596+volantissupreme@users.noreply.github.com>
2023-04-23 03:08:33 -0700

    Push

--- src/main/java/com/ruse/world/content/DryStreak.java
+++ src/main/java/com/ruse/world/content/DryStreak.java
@@ -88,6 +88,7 @@ public class DryStreak {
         FIXED_DRY_STREAK_MILESTONE.put(188, 10000); //hound
         FIXED_DRY_STREAK_MILESTONE.put(1311, 12500); //hound
         FIXED_DRY_STREAK_MILESTONE.put(1313, 15000); //hound
+        FIXED_DRY_STREAK_MILESTONE.put(1318, 17000); //hound
     }
 
     public DryStreak(Player player) {
