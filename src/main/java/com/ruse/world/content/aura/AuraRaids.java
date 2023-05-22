82a23c76c648da733f72ecacadcdf86b3a55521c
MyBits Studio <98053343+MyBitsStudio@users.noreply.github.com>
2023-05-22 02:22:46 -0700

    Battle pass Update

--- src/main/java/com/ruse/world/content/aura/AuraRaids.java
+++ src/main/java/com/ruse/world/content/aura/AuraRaids.java
@@ -387,7 +387,7 @@ public class AuraRaids {
                     Box[] loot = AuraRaidLoot.LOOT;
 
                     player.getPointsHandler().incrementANGELKILLCount(1);
-                    player.getSeasonPass().incrementExp(8600, false);
+                    player.getSeasonPass().incrementExp(12600, false);
 
                     Box drop = getLoot(loot, party.getPlayers().size());
 
