f7075be47cd32db734c03421a4ce6597fcee7797
MyBits Studio <98053343+MyBitsStudio@users.noreply.github.com>
2023-05-26 15:23:00 -0700

    New Instance System Testing

--- src/main/java/com/ruse/engine/task/impl/NPCDeathTask.java
+++ src/main/java/com/ruse/engine/task/impl/NPCDeathTask.java
@@ -350,14 +350,16 @@ public class NPCDeathTask extends Task {
 
                                         }
 
+
+
                                     }
 
-
-                                    if(npc.getInstance() != null){
-                                        npc.getInstance().remove(npc);
-                                        super.stop();
+                                    if(killer != null){
+                                        if(killer.getInstance() != null){
+                                            killer.getInstance().remove(npc);
+                                            super.stop();
+                                        }
                                     }
-
 
                                     if (npc.getDefinition().getRespawnTime() > 0 && npc.getLocation() != Location.GRAVEYARD && npc.getLocation() != Location.KEEPERS_OF_LIGHT_GAME
                                             && npc.getLocation() != Location.DUNGEONEERING && npc.getLocation() != Location.CUSTOM_RAIDS && !npc.isEventBoss()) {
