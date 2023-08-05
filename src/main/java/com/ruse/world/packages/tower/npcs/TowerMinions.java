a7d7e56ddc07ee2d082b9abc7535cc3c025d5b59
MyBits Studio <98053343+MyBitsStudio@users.noreply.github.com>
2023-08-05 15:21:30 -0700

    -- More starter monsters/bosses/drops -- Changed Packs to Tier Packs

--- src/main/java/com/ruse/world/packages/tower/npcs/TowerMinions.java
+++ src/main/java/com/ruse/world/packages/tower/npcs/TowerMinions.java
@@ -2,6 +2,7 @@ package com.ruse.world.packages.tower.npcs;
 
 import com.ruse.model.Position;
 import com.ruse.world.entity.impl.npc.NPC;
+import com.ruse.world.entity.impl.player.Player;
 
 public class TowerMinions extends NPC {
     public TowerMinions(int id, Position position) {
@@ -10,6 +11,15 @@ public class TowerMinions extends NPC {
 
     public void buff(int[] buffs){
 
+    }
+
+    @Override
+    public void onDeath(){
+        Player player = getCombatBuilder().getLastAttacker().asPlayer();
+        if(player != null){
+            player.getTower().getInstance().check(player);
+        }
+        super.onDeath();
     }
 
 
