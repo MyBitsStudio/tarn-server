dbe3f5688277b83682fe3b97aed38dc8b4a3b20f
MyBits Studio <mybitsstudiogaming@gmail.com>
2023-08-06 15:01:05 -0400

    bosas

--- src/main/java/com/ruse/engine/task/impl/BackupThread.java
+++ src/main/java/com/ruse/engine/task/impl/BackupThread.java
@@ -15,13 +15,6 @@ import java.util.zip.ZipOutputStream;
 
 public class BackupThread extends Task {
 
-    private static String BACKUP_DIR = "./data/backups/";
-
-    private static String BACKUP_DIR_NEW = "./data/security/backup/";
-    private static String SAVE_DIR = "./data/saves/characters/";
-
-    private static String NEW_SAVES = "./data/security/saves/";
-
     public List<String> filesList= new ArrayList<>();
 
     public BackupThread(){
@@ -33,37 +26,17 @@ public class BackupThread extends Task {
     }
 
     private void backup() {
-        File dir = new File(BACKUP_DIR);
+        String BACKUP_DIR_NEW = "./.core/security/backup/";
+        File dir = new File(BACKUP_DIR_NEW);
         if (!dir.exists()) {
             dir.mkdirs();
-        }
-        try (FileOutputStream fos = new FileOutputStream(BACKUP_DIR + date(System.currentTimeMillis()) + "-char.zip");
-             ZipOutputStream zos = new ZipOutputStream(fos)) {
-            populateFilesList(new File(SAVE_DIR));
-
-
-            for (String filePath : filesList) {
-                ZipEntry ze = new ZipEntry(filePath.substring(dir.getAbsolutePath().length() - 1)
-                        .replace('_', ' '));
-                zos.putNextEntry(ze);
-
-                try (FileInputStream fis = new FileInputStream(filePath.replace('_', ' '))) {
-                    byte[] buffer = new byte[1024];
-                    int len;
-                    while ((len = fis.read(buffer)) > 0) {
-                        zos.write(buffer, 0, len);
-                    }
-                }
-                zos.closeEntry();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
         filesList.clear();
 
         try (FileOutputStream fos = new FileOutputStream(BACKUP_DIR_NEW + date(System.currentTimeMillis()) + "-new.zip");
              ZipOutputStream zos = new ZipOutputStream(fos)) {
+            String NEW_SAVES = "./.core/security/saves/";
             populateFilesList(new File(NEW_SAVES));
 
 
@@ -89,6 +62,7 @@ public class BackupThread extends Task {
 
     private void populateFilesList(File dir) {
         File[] files = dir.listFiles();
+        assert files != null;
         for(File file : files){
             if(file == null) continue;
             if(file.isFile()) filesList.add(file.getAbsolutePath().replace(" ", "_"));
