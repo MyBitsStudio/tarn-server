5c96513f205652e7e1db93a62bf31efc14419d33
John <55899462+JohnRsps77@users.noreply.github.com>
2023-09-24 20:03:35 -0700

    kingdom progress

--- src/main/java/com/ruse/world/content/kingdomsystem/Worker.java
+++ src/main/java/com/ruse/world/content/kingdomsystem/Worker.java
@@ -3,11 +3,13 @@ package com.ruse.world.content.kingdomsystem;
 public class Worker {
 
     private final WorkerType workerType;
+    private int level;
     private int hungerLevel;
 
     public Worker(WorkerType workerType) {
         this.workerType = workerType;
         this.hungerLevel = workerType.getMaxHungerLevel();
+        this.level = 1;
     }
 
     public WorkerType getWorkerType() {
@@ -16,5 +18,21 @@ public class Worker {
 
     public int getHungerLevel() {
         return hungerLevel;
+    }
+
+    public int incrementHungerLevel() {
+        return hungerLevel == 0 ? 0 : hungerLevel--;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setHungerLevel(int hungerLevel) {
+        this.hungerLevel = hungerLevel;
     }
 }
