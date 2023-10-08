baf3e8021098189a4463939c9f61bf967db7caba
MyBits <mybitsstudiogaming@gmail.com>
2023-10-08 16:31:29 -0400

    tt

--- src/main/java/com/ruse/world/packages/mode/impl/UltimateIronman.java
+++ src/main/java/com/ruse/world/packages/mode/impl/UltimateIronman.java
@@ -30,7 +30,7 @@ public class UltimateIronman extends Gamemode {
     @Override
     public boolean canOpenShop(int shopId) {
         switch (shopId) {
-            case 1, 7, 4, 6, 5, 0, 8, 2, 3:
+            case 1, 7, 4, 6, 5, 0, 8, 2, 3, 9:
                 return true;
         }
         return false;
@@ -43,7 +43,7 @@ public class UltimateIronman extends Gamemode {
 
     @Override
     public boolean canGetRewards() {
-        return false;
+        return true;
     }
 
     @Override
