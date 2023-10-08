ed66519975cd9ba788a679ce0388a54b48a35e8c
Quantum <mybitsstudiogaming@gmail.com>
2023-10-08 12:54:28 -0700

    -- content update

--- src/main/java/com/ruse/world/packages/skills/crafting/Craft.java
+++ src/main/java/com/ruse/world/packages/skills/crafting/Craft.java
@@ -6,6 +6,7 @@ import com.ruse.model.Skill;
 import com.ruse.model.definitions.ItemDefinition;
 import com.ruse.util.Misc;
 import com.ruse.world.entity.impl.player.Player;
+import com.ruse.world.packages.seasonpass.SeasonPassManager;
 
 public class Craft {
 
@@ -129,9 +130,15 @@ public class Craft {
                             player.getInventory().add(new Item(product.getFinalProduct()));
                             player.getSkillManager().addExperience(Skill.CRAFTING, product.getXp());
                             player.sendMessage("You have crafted a " + ItemDefinition.forId(product.getFinalProduct()).getName() + ".");
+                            player.getSeasonPass().incrementExp(216 * product.getXp(), false);
                         } else {
                             player.sendMessage("You have failed to craft the item.");
                         }
+                    } else {
+                        player.getInventory().add(new Item(product.getFinalProduct()));
+                        player.getSkillManager().addExperience(Skill.CRAFTING, product.getXp());
+                        player.sendMessage("You have crafted a " + ItemDefinition.forId(product.getFinalProduct()).getName() + ".");
+                        player.getSeasonPass().incrementExp(216 * product.getXp(), false);
                     }
 
                     player.getPA().sendFrame126("Required Level: " + product.getReqLevel(), 47262);
